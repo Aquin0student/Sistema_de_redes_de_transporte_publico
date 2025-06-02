439d5a5c9d04c36abf130d09d12c9609d7ee806b
ViniciusCasF <vivifilaretti@gmail.com>
2025-06-02 16:46:57 -0300

    commit 1

---
+++ Linha.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Linha {
+    private String id;
+    private String nome;
+    List <Vertice> estacoes;
+    List <Aresta> caminhos;
+    int frequencia_Trens;
+}
